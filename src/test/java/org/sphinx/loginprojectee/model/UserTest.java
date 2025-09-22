6a85bc4b16f65a4a7ffe18df682ce8d1d5981a67
Ali Nejati <alinejati650@gmail.com>
2025-09-22 22:42:24 +0330

    Test created and tested data entery works fine

--- src/test/java/org/sphinx/loginprojectee/model/UserTest.java
+++ src/test/java/org/sphinx/loginprojectee/model/UserTest.java
@@ -2,12 +2,15 @@ package org.sphinx.loginprojectee.model;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.sphinx.loginprojectee.util.HibernateUtil;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class UserTest {
+public class UserTest {
 
     @Test
     void testUser() {
@@ -30,6 +33,44 @@ class UserTest {
         transaction.commit();
         session.close();
 
+    }
+    @Test
+    void testFetchingIdOne(){
+        Session session = HibernateUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+
+        List<User> users = session.createQuery("from User").list();
+        User user = (User) users.get(0);
+        transaction.commit();
+
+        session.close();
+
+        assertEquals("Ali", user.getFirstName());
+        assertEquals("Nejati", user.getLastName());
+        assertEquals("AliNejati", user.getUsername());
+        assertEquals("123", user.getPassword());
+        assertEquals("aliNejati@gmail.com", user.getEmail());
+    }
+    @BeforeAll
+    public static void setupDatabase(){
+        // create 5 test users
+        User ali = new User("Ali", "Nejati", "AliNejati", "123", "aliNejati@gmail.com");
+        User sara = new User("Sara", "Ahmadi", "SaraAh", "123", "sara@gmail.com");
+        User reza = new User("Reza", "Karimi", "RezaK", "123", "reza@gmail.com");
+        User neda = new User("Neda", "Shahri", "NedaS", "123", "neda@gmail.com");
+        User omid = new User("Omid", "Moradi", "OmidM", "123", "omid@gmail.com");
+        Session session = HibernateUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+        //deleting previous Datas in DB
+        session.createQuery("delete from User").executeUpdate();
+        //inserting managed datas in DB
+        session.persist(ali);
+        session.persist(sara);
+        session.persist(reza);
+        session.persist(neda);
+        session.persist(omid);
+        transaction.commit();
+        session.close();
     }
 
 }
